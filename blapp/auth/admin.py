e2f7894c5bd64205005958ff38c5710422fbd96c
Olle Vidner <olle@vidner.se>
2018-08-31 23:31:45 +0200

    Added user account admin

--- blapp/auth/admin.py
+++ blapp/auth/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 from . import models
 
 
+@admin.register(models.ServiceAccount)
 class ServiceAccountAdmin(admin.ModelAdmin):
     readonly_fields = ["token"]
 
 
-admin.site.register(models.ServiceAccount, ServiceAccountAdmin)
+@admin.register(models.UserAccount)
+class UserAccountAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("person",)}),
+        (
+            _("Permissions"),
+            {
+                "classes": ["collapse"],
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    list_display = ("username", "email", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = (
+        "username",
+        "person__first_name",
+        "person__last_name",
+        "person__email",
+    )
