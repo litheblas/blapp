0148019438a3e29e18ea11601ea36b322d7e4144
Olle Vidner <olle@vidner.se>
2018-09-03 11:37:09 +0200

    Un-broke tests

--- tests/people/test_models.py
+++ tests/people/test_models.py
@@ -8,11 +8,21 @@ pytestmark = pytest.mark.django_db
 
 class test_person:
     def test_unique_email(self):
-        factories.PersonFactory.create(email="foo@bar.com")
+        p1 = factories.PersonFactory(email="foo@bar.com")
+        p1.full_clean()
+        p1.save()
 
         with pytest.raises(ValidationError):
-            factories.PersonFactory.create(email="fOo@BAR.com")
+            p2 = factories.PersonFactory(email="fOo@BAR.com")
+            p2.full_clean()
+            p2.save()
 
     def test_non_unique_email(self):
-        assert factories.PersonFactory.create(email="")
-        assert factories.PersonFactory.create(email=None)
+        p1 = factories.PersonFactory(email="")
+        p1.full_clean()
+        p1.save()
+        assert p1
+        p2 = factories.PersonFactory(email=None)
+        p2.full_clean()
+        p2.save()
+        assert p2
