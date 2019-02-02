17ee6f0ea22defd1ee07570ffb5bbdbd0070caa1
Olle Vidner <olle@vidner.se>
2019-02-02 20:04:13 +0100

    Ping actual server for connectivity status

--- blapp/frontend/src/commerce/index.tsx
+++ blapp/frontend/src/commerce/index.tsx
@@ -4,8 +4,10 @@ import { Provider } from 'react-redux'
 import { applyMiddleware, compose, createStore, StoreEnhancer } from 'redux'
 import { offline } from '@redux-offline/redux-offline'
 import offlineConfig from '@redux-offline/redux-offline/lib/defaults'
+import { NetworkCallback } from '@redux-offline/redux-offline/lib/types'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import isReachable from 'is-reachable'
 
 import { AppContainer } from 'blapp/commerce/containers/App'
 import reducer from 'blapp/commerce/reducers'
@@ -15,9 +17,34 @@ const persistCallback = () => {
   store.subscribe(() => doRender())
 }
 
+const detectNetwork = (callback: NetworkCallback) => {
+  const handle = (status: boolean) => {
+    // Copy-ished from original redux-offline implementation
+    if (window.requestAnimationFrame) {
+      window.requestAnimationFrame(() => callback(status))
+    } else {
+      setTimeout(() => callback(status), 0)
+    }
+  }
+
+  const checkNetwork = () => {
+    if (navigator.onLine === false) {
+      handle(false)
+    }
+
+    isReachable(location.origin, {timeout: 5000}).then(handle)
+  }
+
+  window.addEventListener("online", checkNetwork)
+  window.addEventListener("offline", checkNetwork)
+  setInterval(checkNetwork, 60000)
+  checkNetwork()
+}
+
 const offlineConf = {
   ...offlineConfig,
   persistCallback,
+  detectNetwork,
 }
 
 const store = createStore(
