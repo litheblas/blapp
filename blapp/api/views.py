1b4242d584ede3b2176b5ba48ff2cdd694134b4e
Oscar Falk <oscfa693@student.liu.se>
2022-10-24 16:53:52 +0200

    /api/graphql is now protected.

--- blapp/api/views.py
+++ blapp/api/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from graphene_django.views import GraphQLView
 
 from blapp.auth.models import ServiceAccount
@@ -11,11 +12,12 @@ _api_view = GraphQLView.as_view(schema=schema, graphiql=True)
 
 
 @csrf_exempt
+@login_required
 def api_view(request):
-    try:
-        service_account = ServiceAccount.objects.get(token=token_from_request(request))
-        request.user = service_account
-    except ServiceAccount.DoesNotExist as exc:
-        return HttpResponseBadRequest("Meh. Please log in.", status=401)
+    # try:
+    #     service_account = ServiceAccount.objects.get(token=token_from_request(request))
+    #     request.user = service_account
+    # except ServiceAccount.DoesNotExist as exc:
+    #     return HttpResponseBadRequest("Meh. Please log in.", status=401)
 
     return _api_view(request)
