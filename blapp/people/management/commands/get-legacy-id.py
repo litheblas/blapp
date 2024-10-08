239542d5b740d3eb41e8d3dde50fc1f2ea1aa06b
Eb-dur <malte.hammar01@gmail.com>
2024-10-08 19:42:18 +0200

    add support for wiki ID:s

--- blapp/people/management/commands/get-legacy-id.py
+++ blapp/people/management/commands/get-legacy-id.py
@@ -1,15 +1,21 @@
 import json
 from django.core.management.base import BaseCommand, CommandError
 from blapp.people.models import Person
+from blapp.auth.models import UserAccount
 
 class Command(BaseCommand):
     help = "create json file with legacy id and blapp id for all users"
 
     def handle(self, *args, **options):
         persons = Person.objects.all()
+        useraccounts = UserAccount.objects.all()
         data = {}
         for person in persons:
-            data[str(person.legacy_id)] = str(person.id)
+            this_account = useraccounts.get(person=person)
+            if this_account:
+                data[str(person.legacy_id)] = str(this_account.id)
+            else:
+                data[str(person.legacy_id)] = str(person.id)
         with open("legacy_id_to_blapp_id.json", "w") as f:
             json.dump(data, f)
         data = {}
